90eda3812e0cafd9003f7bd9e7a80ff0668cb400
Diogo Alves <diogo.alves.ti@gmail.com>
2021-10-09 10:33:53 -0300

    Register the Author model on Django admin

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
